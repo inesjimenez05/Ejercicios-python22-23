54144c64f4eaa74c9fba6c61c29f75d307e1a67c
Inés <al_ijimenezg@iessierradelasvillas.org>
2022-09-29 10:03:15 +0200

    Comentarios, entrada, salida datos y tipos de datos en Python

---
+++ Holamundo.py
@@ -0,0 +1,14 @@
+nombre="Inés"
+edad= 17
+mayorOMenor= False
+
+#tipos de da
+#entrada y salida de datos
+#salida con print
+
+nombre= input("Dime tu nombre: \n")
+edad= input("Dime tu edad: \n")
+
+#\n: salto de línea
+
+print("Buenos días", nombre, "tu edad es", edad)
