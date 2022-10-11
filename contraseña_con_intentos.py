afe7dc09ad705937be049784f9e20c338f56c248
Inés Jiménez <al_ijimenezg@iessierradelasvillas.org>
2022-10-11 17:58:50 +0200

    Terminado bucle con intentos

---
+++ contraseña_con_intentos.py
@@ -0,0 +1,14 @@
+#ejercicio contraseña con intentos
+
+codigo= "hola"
+valor=""
+intentos=0
+
+while(intentos <= 2):
+    intentos = intentos + 1 
+    print("Error repita la contraseña")
+    valor=input("Diga la contraseña \n")
+    if(codigo == valor ):
+       intentos=4
+       print("Constraseña correcta")
+       
