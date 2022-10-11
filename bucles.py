78fb47b55a6782ac06f0e8a78e053534d841aa47
Inés <al_ijimenezg@iessierradelasvillas.org>
2022-10-11 14:40:24 +0200

    Bucles y codigo

---
+++ bucles.py
@@ -0,0 +1,13 @@
+#bucle
+
+numero=0
+i=0
+Bandera =True
+numero=int(input("Dime un número \n"))
+
+
+while(i <=10):
+
+    print(numero, "*", i, "=", (numero*i))
+    i= i+1
+
---
+++ ejemplo_contraseña.py
@@ -0,0 +1,12 @@
+#ej_codigo
+
+codigo= "hola"
+valor=""
+
+valor=input("Diga la contraseña \n")
+
+while(codigo != valor ):
+    print("Error repita la contraseña")
+    valor=input("Diga la contraseña \n")
+
+print("Contraseña corecta")
