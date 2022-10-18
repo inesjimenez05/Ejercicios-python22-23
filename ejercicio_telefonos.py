a9eb65472de9bf5fa35115ccea83634aef1850d0
Inés <al_ijimenezg@iessierradelasvillas.org>
2022-10-18 14:41:17 +0200

    Editado

--- ejercicio_telefonos.py
+++ ejercicio_telefonos.py
@@ -1,20 +1,18 @@
-vContactos=[]
-num=3
-while(num!=5):
+#Función que pinta el menú del 1 al 5
 
-    print("1. Insertar contacto")
-    print("2. Borrar contactos")
-    print("3. Buscar contactos")
-    print("4. Ver todos los contactos")
-    print("5. Salir del programa")
+def menuNumeros():
+    num=3
 
-    try:
-        num=int(input("Diga un numero: \n"))
-    except:
-        print("Las opciones son de la 1 a la 5")
-    
-    '''
-    if(num==1):
-        print("Insertar contacto")
-    if(num==2):
-    '''
+    while(num<1 or num>5):
+        print("1. Insertar contacto")
+        print("2. Borrar contactos")
+        print("3. Buscar contactos")
+        print("4. Ver todos los contactos")
+        print("5. Salir del programa")
+        try:
+            num=int(input("Di un número: \n"))
+        except:
+            print("Dime un numero del 1 al 5")
+    return num
+
+num=menuNumeros()
---
+++ funcion.py
@@ -0,0 +1,8 @@
+#funciones
+
+def sumar_dos_numeros(num1:int, num2:int):
+    #print("La suma de los numeros es", num1+num2)
+    return num1+num2
+
+suma=sumar_dos_numeros(4,5)
+print("La suma es", suma)
