5936b429a537ec75216143b34f3cc86ff2144975
Inés <al_ijimenezg@iessierradelasvillas.org>
2022-11-18 11:14:23 +0100

    Viernes 18

--- ejerciciotkinter.py
+++ ejerciciotkinter.py
@@ -1,8 +1,24 @@
 from tkinter import *
 from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+
+#Generar la ventana para pintar componentes
+ventana = Tk()
+ventana.title("Primer ejercicio")
+ventana.geometry("200x300")
+ventana.resizable(width=False, height=False)
+ventana.config(background="purple")
+
+
+#Genera lienzo para pintar componentes
+
+frm = ttk.Frame(ventana, padding=10).pack()
+
+
+#Componentes label y button
+LabelTexto= ttk.Label(frm, text="Hola Tkinter!")
+LabelTexto.config(background="Green", border=5, font=("Arial",15))
+LabelTexto.pack()
+
+ttk.Button(frm, text="Salir", command=ventana.destroy).pack()
+
+ventana.mainloop()
